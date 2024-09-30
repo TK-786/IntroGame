682866d9ab62ed082b418488639eb7993617a648
naveed <ec22837@qmul.ac.uk>
2024-09-30 13:23:31 +0100

    added debug info

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -12,11 +12,17 @@ public class PlayerController : MonoBehaviour {
     private int numPickups = 4;
     public TextMeshProUGUI scoreText ;
     public TextMeshProUGUI winText ;
+    public TextMeshProUGUI PlayerVelocity ;
+    public TextMeshProUGUI PlayerPosition ;
+    public Vector2 pos ;
+    public Vector2 newPos ;
+    public float velocity ;
 
     void Start() {
         count = 0;
         winText.text = "" ;
         SetCountText() ;
+        pos = transform.position ;
     }
 
 
@@ -24,10 +30,23 @@ public class PlayerController : MonoBehaviour {
         moveValue = value.Get<Vector2>() ;
     }
 
+    // void Update () {
+    //     newPos = transform.position ;
+    //     velocity = (newPos - pos).magnitude/Time.deltaTime ;
+    //     PlayerPosition.text = "Position: " + newPos.ToString() ;
+    //     PlayerVelocity.text = "velocity :" + velocity.ToString() ;
+    //     pos = newPos ;
+    // }
+
     void FixedUpdate () {
         Vector3 movement = new Vector3 ( moveValue.x , 0.0f , moveValue.y ) ;
 
         GetComponent<Rigidbody>().AddForce( movement * speed * Time.fixedDeltaTime ) ;
+        newPos = transform.position ;
+        velocity = (newPos - pos).magnitude/Time.deltaTime ;
+        PlayerPosition.text = "Position: " + newPos.ToString() ;
+        PlayerVelocity.text = "velocity :" + velocity.ToString() ;
+        pos = newPos ;
     }
 
     void OnTriggerEnter (Collider other) {
